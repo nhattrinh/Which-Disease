723f80f770271816f1d5ae117afbf21c5b44aa84
Nhat Trinh <goodyshoes_nt@yahoo.com>
2018-11-18 17:58:33 -0800

    filenames updated, changed classification iterations, updated output

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os, sys, re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
 
 def main():
     test_docs = []
@@ -11,14 +11,16 @@ def main():
     vectorizer = CountVectorizer()
     transformer = TfidfTransformer(use_idf=False)
 
-    with open('train-1.dat','r') as infile:
+    with open('train.dat','r') as infile:
         data_split = [line.split('\t') for line in infile.read().splitlines()]
         for d in data_split:
             train_docs.append(d[1])
             train_classes.append(d[0])
+        infile.close()
 
-    with open('test-1.dat','r') as infile:
+    with open('test.dat','r') as infile:
         test_docs.extend(infile.read().splitlines())
+        infile.close()
 
     train_counts = vectorizer.fit_transform(train_docs,None)
     test_counts = vectorizer.transform(test_docs)
@@ -26,9 +28,13 @@ def main():
     train_tfidf = transformer.fit_transform(train_counts, None)
     test_tfidf = transformer.transform(test_counts)
 
-    classifier = MultinomialNB().fit(train_tfidf, train_classes)
+    classifier = SGDClassifier(tol=None, max_iter=2000).fit(train_tfidf, train_classes)
 
     predictions = classifier.predict(test_tfidf)
+
+    with open('format.dat','w') as outfile:
+        outfile.write('\n'.join(predictions))
+        outfile.close()
 
 if __name__ == '__main__':
     main()
